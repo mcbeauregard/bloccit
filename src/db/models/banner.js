4ffe4c685b575ff564d1218b957335adf74ba274
mcbeauregard <mcbeauregard@gmail.com>
2018-11-10 13:44:18 -0800

    created banner model and associated it with banner

---
+++ src/db/models/banner.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Banner = sequelize.define('Banner', {
+    source: DataTypes.STRING,
+    description: DataTypes.STRING,
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topic",
+        key: "id",
+        as: "topicId",
+      }
+    }
+  }, {});
+  Banner.associate = function(models) {
+    // associations can be defined here
+    Banner.belongsTo(models.Topic, {
+      foreignKey: "topicId", // indicates what foreign key to use in the banners table.
+      onDelete: "CASCADE", // deletes banner object
+    });
+  };
+  return Banner;
+};
--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -6,6 +6,10 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId", // indicates what foreign key to use in the banner table.
+      as: "banners",
+    });
   };
   return Topic;
 };
