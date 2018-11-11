c0cd0eb654adcfed220bbdffd2f3525c9a104ad3
mcbeauregard <mcbeauregard@gmail.com>
2018-11-10 16:53:16 -0800

    topics resource assignment one done

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "topicId", // indicates what foreign key to use in the banner table.
       as: "banners",
     });
+    Topic.hasMany(models.Rule, {
+      foreignKey: "topicId",
+      as: "rules",
+    });
   };
   return Topic;
 };
