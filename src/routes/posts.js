1babeb4123235f9c3126f5cf2a023205a3053883
mcbeauregard <mcbeauregard@gmail.com>
2018-11-17 14:33:57 -0800

    fixed one failuer

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const postController = require("../controllers/postController")
 
 router.get("/topics/:topicId/posts/new", postController.new);
