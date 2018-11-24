b438f467749d406387015531c9bcedb928fbbd8d
mcbeauregard <mcbeauregard@gmail.com>
2018-11-24 15:10:13 -0800

    3 failures

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,7 +6,7 @@ module.exports = {
       const authorized = new Authorizer(req.user).new();
    
        if(authorized) {
-         res.render("posts/new");
+        res.render("posts/new", {topicId: req.params.topicId});
       } else {
          req.flash("notice", "You are not authorized to do that.");
          res.redirect("/posts");
@@ -62,19 +62,18 @@ module.exports = {
     });
 },
 
-      destroy(req, res, next){
-
-            postQueries.deletePost(req, (err, deletedRecordsCount) => {
-              if(err){
-                res.redirect(err, `/topics/${req.params.topicId}/posts/${req.params.id}`)
-              } else {
-                res.redirect(303, `/topics/${req.params.topicId}`)
-              }
-            });
-          },
+destroy(req, res, next){
+  postQueries.deletePost(req.params.id, (err, deletedRecordsCount) => {
+      if(err){
+          res.redirect(500, `/topics/${req.params.topicId}/posts/${req.params.id}`)
+      } else {
+          res.redirect(303, `/topics/${req.params.topicId}`)
+      }
+  });
+},
 
 update(req, res, next){
-  postQueries.updatePost(req, req.body, (err, post) => {
+  postQueries.updatePost(req.params.id, req.body, (err, post) => {
     if(err || post == null){
       res.redirect(401, `/topics/${req.params.topicId}/posts/${req.params.id}/edit`);
     } else {
@@ -84,3 +83,4 @@ update(req, res, next){
 }
 
 }
+//
