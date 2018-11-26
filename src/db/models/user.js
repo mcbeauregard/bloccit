d6a75f036d6aad80d3acad80fff1e93941264ace
mcbeauregard <mcbeauregard@gmail.com>
2018-11-26 15:24:07 -0800

    Checkpoint voting done

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -25,9 +25,15 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "posts"
     });
+
     User.hasMany(models.Comment, {
       foreignKey: "userId",
       as: "comments"
+    });
+
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
     });
   };
   User.prototype.isAdmin = function() {
