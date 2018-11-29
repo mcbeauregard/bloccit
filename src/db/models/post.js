974a2e7aba985437551a956cee547a28b46ec4dc
mcbeauregard <mcbeauregard@gmail.com>
2018-11-29 13:37:10 -0800

    added afterCreate hook

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -47,6 +47,13 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "favorites"
     });
+
+    Post.afterCreate((post, callback) => {
+      return models.Favorite.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
 
   Post.prototype.getFavoriteFor = function(userId){
